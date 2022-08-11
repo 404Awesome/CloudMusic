a26693b90ddd3bd1d77b7497e5d9c0950600c1c6
awesome404 <awesome404@163.com>
2022-08-11 23:15:00 +0800

    chore: 增加了smoothscroll-polyfill依赖!

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import smoothscroll from "smoothscroll-polyfill";
 import ElementPlus from "element-plus";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
 
+smoothscroll.polyfill();
 
 let app = createApp(App);
 app.use(createPinia());

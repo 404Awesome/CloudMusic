522809d0c3614d5c8bf3229c1b6dc49541c2c54f
awesome404 <awesome404@163.com>
2022-08-12 21:44:51 +0800

    chore: 增加事件总线mitt库!

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,12 @@
+import mitt from "mitt";
 import Handle from "./handle";
 import Operate from "./operate";
 import Validation from "./validation";
 
+const useMitt = mitt();
+
 export {
+  useMitt,    // 事件总线
   Handle,     // 处理相关工具类
   Operate,    // 操作相关工具类
   Validation  // 验证相关工具类
